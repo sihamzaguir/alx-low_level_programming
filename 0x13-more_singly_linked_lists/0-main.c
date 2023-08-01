da419ae48fbca057ea27f670d6bcec4a81ee22b1
sihamzaguir <zaguir98@gmail.com>
2023-08-01 16:50:01 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -5,8 +5,8 @@
 
 /**
  * main - check the code
- * 
- * Return: Always 0.
+ *
+ *Return: Always 0.
  */
 int main(void)
 {
@@ -19,8 +19,8 @@ int main(void)
     new = malloc(sizeof(listint_t));
     if (new == NULL)
     {
-        printf("Error\n");
-        return (1);
+	    printf("Error\n");
+	    return (1);
     }
     new->n = 9;
     new->next = head;
